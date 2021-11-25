3464c776defb0b2c2cf095dbcb0887c70b8b6028
luna <lunashoval@gmail.com>
2021-11-25 09:38:44 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const sumbitBtn = document.querySelector(".submitBtn");
+const emailInput = document.querySelector(".email");
+const countryInput = document.querySelector(".country");
+const zipCodeInput = document.querySelector(".zipCode");
+const passwordInput = document.querySelector(".password");
+const passwordConfInput = document.querySelector(".passwordConf");
