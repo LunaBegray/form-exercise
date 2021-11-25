475f6e51c8afa264ace4e0e3ae00b5f70988231c
luna <lunashoval@gmail.com>
2021-11-25 10:52:28 -0500

    script.js

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ const countryInput = document.querySelector(".country");
 const zipCodeInput = document.querySelector(".zipCode");
 const passwordInput = document.querySelector(".password");
 const passwordConfInput = document.querySelector(".passwordConf");
+
+const checkPassEq = function(){
+    if(passwordInput.value != passwordConfInput.value){
+        alert("the password and the confirmation password dont match you fucking dumbass")
+        passwordConfInput.style.border = '2px solid red';
+        passwordInput.style.border = '2px solid red';
+        return false
+    } else {
+        passwordConfInput.style.border = '2px solid green';
+        passwordInput.style.border = '2px solid green';
+        return true
+    }
+}
+
+sumbitBtn.addEventListener('click', function(){
+    checkPassEq();
+    if(checkPassEq() && emailInput.checkValidity() && countryInput.checkValidity() && zipCodeInput.checkValidity()){
+        console.log("the checkPassed and everything is okay so We are erasing the values");
+        emailInput.value = "";
+        countryInput.value = "";
+        zipCodeInput.value = "";
+        passwordInput.value = "";
+        passwordConfInput.value = "";
+        passwordInput.style.border = "2px solid red";
+        passwordConfInput.style.border = "2px solid red"
+    }
+});
